542aab1268b1cb7fa6c50d053648928ed7ceded6
Oxygen033 <102524912+Oxygen033@users.noreply.github.com>
2023-05-04 22:29:37 +0600

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,29 @@
+import unittest
+from app import app
+
+
+class AppTestCase(unittest.TestCase):
+
+    # Проверка, возвращает ли функция ответ вообще
+    def test_response(self):
+        with app.test_client() as client:
+            response = client.post('/', data={'a': 1, 'b': 2, 'c': 1})
+            self.assertEqual(response.status_code, 200)
+
+    # Проверка решения уравнения с действительными корнями (для a=1, b=0, c=-4)
+    def test_real_roots(self):
+        with app.test_client() as client:
+            response = client.post('/', data={'a': 1, 'b': 0, 'c': -4})
+            self.assertIn(b'x1 = 2', response.data)
+            self.assertIn(b'x2 = -2', response.data)
+
+    # Проверка решения уравнения с отрицательным дискриминантом (без действительных корней)
+    def test_complex_roots(self):
+        with app.test_client() as client:
+            response = client.post('/', data={'a': 1, 'b': 2, 'c': 2})
+            self.assertIn(b'x1 = (-1+1j)', response.data)
+            self.assertIn(b'x2 = (-1-1j)', response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
