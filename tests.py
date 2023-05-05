7f80da26d4b00839bf472767e5fd2dc170ed9db0
Oxygen033 <102524912+Oxygen033@users.noreply.github.com>
2023-05-05 07:06:19 +0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class AppTestCase(unittest.TestCase):
     # Проверка, возвращает ли функция ответ вообще
     def test_response(self):
         with app.test_client() as client:
-            response = client.post('/', data={'a': 1, 'b': 2, 'c': 1})
+            response = client.post('/', data={'a': 1, 'b': 1, 'c': 1})
             self.assertEqual(response.status_code, 200)
 
     # Проверка решения уравнения с действительными корнями (для a=1, b=0, c=-4)
